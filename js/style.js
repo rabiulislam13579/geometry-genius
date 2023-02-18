744d74e5e6bd63a5fd9354b7508353de92012342
rabiulislam13579 <rabiul120158@gmail.com>
2023-02-18 16:34:59 +0600

    get input value

--- js/style.js
+++ js/style.js
@@ -25,3 +25,41 @@ function colors() {
 document.getElementById('btn-blog').addEventListener('click',function(){
   window.location.href='blog.html'
 })
+
+
+//function for take value from the input field
+
+function getInputValue(elementId){
+  const inputField=document.getElementById(elementId);
+  const inputFieldString=inputField.value;
+  const inputFieldValue=parseFloat(inputFieldString);
+  return inputFieldValue;
+}
+
+
+//get the value of input field
+
+//triangle
+const triangleFirstInput=getInputValue('triangle-b');
+const triangleSecondInput=getInputValue('triangle-h');
+
+//rectangle
+const rectangleFirstInput=getInputValue('rectangle-w');
+const rectangleSecondInput=getInputValue('rectangle-l');
+
+//Parallelogram
+const parallelogramFirstInput=getInputValue('parallelogram-b');
+const parallelogramSecondInput=getInputValue('parallelogram-h');
+
+//Rhombus
+const rhombusFirstInput=getInputValue('rhombus-d1');
+const rhombusSecondInput=getInputValue('rhombus-d2');
+
+//Pentagon
+const PentagonFirstInput=getInputValue('pentagon-p');
+const pentagonSecondInput=getInputValue('pentagon-b');
+
+//Ellipse
+const ellipseFirstInput=getInputValue('ellipse-a');
+const ellipseSecondInput=getInputValue('ellipse-b');
+console.log(ellipseFirstInput+ellipseSecondInput);
